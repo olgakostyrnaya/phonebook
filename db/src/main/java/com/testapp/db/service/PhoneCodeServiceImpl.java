e6badc74af04d20f957b1e89cd6548e293114a2b
Kostyrnaya <Kostyrnaya@lanit.ru>
2020-08-10 15:09:35 +0500

    Unactive imports deleted.

--- db/src/main/java/com/testapp/db/service/PhoneCodeServiceImpl.java
+++ db/src/main/java/com/testapp/db/service/PhoneCodeServiceImpl.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class PhoneCodeServiceImpl implements PhoneCodeService {
